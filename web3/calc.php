3952fa7a3d539fa40c46d81bba7bb8c79a9b30fe
EJaranilla <201880132@psu.palawan.edu.ph>
2023-03-02 22:28:31 +0800

    jaranilla 2nd commit

--- web3/calc.php
+++ web3/calc.php
@@ -1,19 +1,45 @@
 <html>
 <head><title>Calculation Result</title></head>
 <body>
-    <?php
+<?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $val1 = $_POST["val1"];
+        $val2 = $_POST["val2"];
+        $calc = $_POST["calc"];
         # test if input is_numeric
-        if(1){
+        if(is_numeric($val1) && is_numeric($val2)){
             # test if calc is not null
-            if(1){
+            if(!empty($calc)){
                 # switch statement for value of calc
-                switch(1){
-                    
+                switch($calc){
+                    case "add":
+                        $result = $val1 + $val2;
+                        break;
+                    case "sub":
+                        $result = $val1 - $val2;
+                        break;
+                    case "mul":
+                        $result = $val1 * $val2;
+                        break;
+                    case "div":
+                        if($val2 == 0){
+                            $result = "undefined";
+                        }
+                        else{
+                            $result = $val1 / $val2;
+                        }
+                        break;
                 }
                 echo "Calculation result: $result";
             }
+            else{
+                echo "Please select an arithmetical operation.";
+            }
         }
-        else{ echo "Invalid entry - please retry"; }
-    ?>
+        else{
+            echo "Invalid entry - please Enter all the needed information and retry.";
+        }
+    }
+?>
 </body>
 </html>
